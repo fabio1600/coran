f16a827348b568d7d9dc82bfcef369ddfd4e0caa
Fabio Disi <fdisi@IZS-SARDEGNA.IT>
2026-03-23 20:06:35 +0100

    test firebase copilot

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,48 +1,18 @@
 import UIKit
 import Flutter
-import Firebase
-import FirebaseMessaging
-import UserNotifications
-
-
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        
-        // Inizializza Firebase
-        FirebaseApp.configure()
-        
-        // Richiesta permessi notifiche push
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: { granted, error in
-                    if let error = error {
-                        print("Errore permessi notifiche: \(error)")
-                    }
-                }
-            )
-        } else {
-            let settings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-        }
-        
-        application.registerForRemoteNotifications()
-        
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-    
-    // Ricezione token APNs
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+
+    // Nessun FirebaseApp.configure()
+    // Nessuna richiesta notifiche
+    // Nessun Messaging
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-/*import 'package:firebase_messaging/firebase_messaging.dart';*/
+import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:coran/app/router.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +11,10 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
 
   
 
