ba21790f585d8cf018248f238d21eff5158ec13f
Fabio Disi <fdisi@IZS-SARDEGNA.IT>
2026-03-23 10:56:26 +0100

    secondo test firebase,aggiunta core

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-/*import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebase_core/firebase_core.dart';*/
+/*import 'package:firebase_messaging/firebase_messaging.dart';*/
+import 'package:firebase_core/firebase_core.dart';
 import 'package:coran/app/router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,6 +15,7 @@ void main() async {
 
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler); // 👈 AGGIUNGI
 */
+await Firebase.initializeApp();
   runApp(
     ProviderScope(
       child: MyApp(),
